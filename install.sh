678ce10571a7c1a0bc0f143d5acb863b22867080
di-sukharev <dim.sukharev@gmail.com>
2023-11-26 17:19:37 +0800

    fixing install script

--- install.sh
+++ install.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Reset
+Color_Off=''
+
+# Regular Colors
+Red=''
+Green=''
+Dim='' # White
+
+# Bold
+Bold_White=''
+Bold_Green=''
+
+if [[ -t 1 ]]; then
+    # Reset
+    Color_Off='\033[0m' # Text Reset
+
+    # Regular Colors
+    Red='\033[0;31m'   # Red
+    Green='\033[0;32m' # Green
+    Dim='\033[0;2m'    # White
+
+    # Bold
+    Bold_Green='\033[1;32m' # Bold Green
+    Bold_White='\033[1m'    # Bold White
+fi
+
+error() {
+    echo -e "${Red}error${Color_Off}:" "$@" >&2
+    exit 1
+}
+
+info() {
+    echo -e "${Dim}$@ ${Color_Off}"
+}
+
+info_bold() {
+    echo -e "${Bold_White}$@ ${Color_Off}"
+}
+
+success() {
+    echo -e "${Green}$@ ${Color_Off}"
+}
+
 # [Initialize color codes and helper functions as in bun.sh]
 
 # Check for bun
@@ -41,7 +84,7 @@ EOF
 chmod +x "$exe" ||
     error 'Failed to set permissions on aitdd executable'
 
-# [Optional: Provide instructions for adding $bin_dir to PATH]
+export PATH="$bin_dir:$PATH"
 
 # Success message
 success "aitdd was installed successfully"
