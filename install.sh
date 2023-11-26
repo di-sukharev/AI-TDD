5c54a7ec5e983b855701a089f7d486c802d58b20
di-sukharev <dim.sukharev@gmail.com>
2023-11-26 17:57:19 +0800

    fix the installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Color codes and helper functions
-Color_Off='\033[0m' # Text Reset
-Red='\033[0;31m'   # Red
-Green='\033[0;32m' # Green
-Dim='\033[0;2m'    # White (Dimmed)
-Bold_White='\033[1m'    # Bold White
-Bold_Green='\033[1;32m' # Bold Green
+if [[ ${OS:-} = Windows_NT ]]; then
+    echo 'error: Please install aitdd using Windows Subsystem for Linux'
+    exit 1
+fi
+
+# Reset
+Color_Off=''
+
+# Regular Colors
+Red=''
+Green=''
+Dim='' # White
+
+# Bold
+Bold_White=''
+Bold_Green=''
+
+if [[ -t 1 ]]; then
+    # Reset
+    Color_Off='\033[0m' # Text Reset
+
+    # Regular Colors
+    Red='\033[0;31m'   # Red
+    Green='\033[0;32m' # Green
+    Dim='\033[0;2m'    # White
+
+    # Bold
+    Bold_Green='\033[1;32m' # Bold Green
+    Bold_White='\033[1m'    # Bold White
+fi
 
 error() {
     echo -e "${Red}error${Color_Off}:" "$@" >&2
@@ -26,59 +49,205 @@ success() {
     echo -e "${Green}$@ ${Color_Off}"
 }
 
-# Check for bun
-command -v bun >/dev/null || error 'bun is required to run aitdd. Please install bun first.'
+GITHUB=${GITHUB-"https://github.com"}
 
-# Define the GitHub repository URL and download URL for your CLI
-github_repo="https://github.com/di-sukharev/AI-TDD"
-aitdd_uri="$github_repo/releases/latest/download/out.zip"
+github_repo="$GITHUB/di-sukharev/ai-tdd"
 
-# Define the installation directory and executable
-install_dir=${HOME}/.aitdd
+exe_name=aitdd
+
+if [[ $# = 0 ]]; then
+    aitdd_uri=$github_repo/releases/latest/download/out.zip
+else
+    aitdd_uri=$github_repo/releases/download/$1/out.zip
+fi
+
+install_env=AITDD_INSTALL
+bin_env=\$$install_env/bin
+
+install_dir=${!install_env:-$HOME/.aitdd}
 bin_dir=$install_dir/bin
 exe=$bin_dir/aitdd
 
-# Create the installation directory if it doesn't exist
-mkdir -p "$bin_dir" || error "Failed to create install directory \"$bin_dir\""
+if [[ ! -d $bin_dir ]]; then
+    mkdir -p "$bin_dir" ||
+        error "Failed to create install directory \"$bin_dir\""
+fi
 
-# Download and extract your CLI files
-curl --fail --location --output "$exe.zip" "$aitdd_uri" || error "Failed to download aitdd from \"$aitdd_uri\""
-unzip -oqd "$install_dir" "$exe.zip" || error 'Failed to extract aitdd'
+curl --fail --location --progress-bar --output "$exe.zip" "$aitdd_uri" ||
+    error "Failed to download aitdd from \"$aitdd_uri\""
 
-# Create the aitdd wrapper command
-cat <<EOF >"$exe"
-#!/usr/bin/env bash
-set -euo pipefail
-bun $install_dir/cli.js "\$@"
-EOF
+unzip -oqd "$bin_dir" "$exe.zip" ||
+    error 'Failed to extract aitdd'
 
-# Make the wrapper executable
-chmod +x "$exe" || error 'Failed to set permissions on aitdd executable'
+mv "$bin_dir/aitdd-$target/$exe_name" "$exe" ||
+    error 'Failed to move extracted aitdd to destination'
 
-# Update shell configuration to include aitdd in PATH
-update_path() {
-    local config_file=$1
-    echo -e "\n# ai-tdd\nexport AI_TDD_INSTALL=\"$install_dir\"\nexport PATH=\"\$AI_TDD_INSTALL/bin:\$PATH\"" >> "$config_file"
-    success "Updated $config_file with ai-tdd PATH"
+chmod +x "$exe" ||
+    error 'Failed to set permissions on aitdd executable'
+
+rm -r "$bin_dir/aitdd-$target" "$exe.zip"
+
+tildify() {
+    if [[ $1 = $HOME/* ]]; then
+        local replacement=\~/
+
+        echo "${1/$HOME\//$replacement}"
+    else
+        echo "$1"
+    fi
 }
 
-# Detect the user's shell and update the corresponding configuration file
-case "${SHELL##*/}" in
+success "aitdd was installed successfully to $Bold_Green$(tildify "$exe")"
+
+if command -v aitdd >/dev/null; then
+    # Install completions, but we don't care if it fails
+    IS_AITDD_AUTO_UPDATE=true $exe completions &>/dev/null || :
+
+    echo "Run 'aitdd --help' to get started"
+    exit
+fi
+
+refresh_command=''
+
+tilde_bin_dir=$(tildify "$bin_dir")
+quoted_install_dir=\"${install_dir//\"/\\\"}\"
+
+if [[ $quoted_install_dir = \"$HOME/* ]]; then
+    quoted_install_dir=${quoted_install_dir/$HOME\//\$HOME/}
+fi
+
+echo
+
+case $(basename "$SHELL") in
 fish)
-    update_path "$HOME/.config/fish/config.fish"
+    # Install completions, but we don't care if it fails
+    IS_AITDD_AUTO_UPDATE=true SHELL=fish $exe completions &>/dev/null || :
+
+    commands=(
+        "set --export $install_env $quoted_install_dir"
+        "set --export PATH $bin_env \$PATH"
+    )
+
+    fish_config=$HOME/.config/fish/config.fish
+    tilde_fish_config=$(tildify "$fish_config")
+
+    if [[ -w $fish_config ]]; then
+        {
+            echo -e '\n# aitdd'
+
+            for command in "${commands[@]}"; do
+                echo "$command"
+            done
+        } >>"$fish_config"
+
+        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
+
+        refresh_command="source $tilde_fish_config"
+    else
+        echo "Manually add the directory to $tilde_fish_config (or similar):"
+
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
     ;;
 zsh)
-    update_path "$HOME/.zshrc"
+    # Install completions, but we don't care if it fails
+    IS_AITDD_AUTO_UPDATE=true SHELL=zsh $exe completions &>/dev/null || :
+
+    commands=(
+        "export $install_env=$quoted_install_dir"
+        "export PATH=\"$bin_env:\$PATH\""
+    )
+
+    zsh_config=$HOME/.zshrc
+    tilde_zsh_config=$(tildify "$zsh_config")
+
+    if [[ -w $zsh_config ]]; then
+        {
+            echo -e '\n# aitdd'
+
+            for command in "${commands[@]}"; do
+                echo "$command"
+            done
+        } >>"$zsh_config"
+
+        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_zsh_config\""
+
+        refresh_command="exec $SHELL"
+    else
+        echo "Manually add the directory to $tilde_zsh_config (or similar):"
+
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
     ;;
 bash)
-    update_path "$HOME/.bashrc"
-    update_path "$HOME/.bash_profile"
+    # Install completions, but we don't care if it fails
+    IS_AITDD_AUTO_UPDATE=true SHELL=bash $exe completions &>/dev/null || :
+
+    commands=(
+        "export $install_env=$quoted_install_dir"
+        "export PATH=$bin_env:\$PATH"
+    )
+
+    bash_configs=(
+        "$HOME/.bashrc"
+        "$HOME/.bash_profile"
+    )
+
+    if [[ ${XDG_CONFIG_HOME:-} ]]; then
+        bash_configs+=(
+            "$XDG_CONFIG_HOME/.bash_profile"
+            "$XDG_CONFIG_HOME/.bashrc"
+            "$XDG_CONFIG_HOME/bash_profile"
+            "$XDG_CONFIG_HOME/bashrc"
+        )
+    fi
+
+    set_manually=true
+    for bash_config in "${bash_configs[@]}"; do
+        tilde_bash_config=$(tildify "$bash_config")
+
+        if [[ -w $bash_config ]]; then
+            {
+                echo -e '\n# aitdd'
+
+                for command in "${commands[@]}"; do
+                    echo "$command"
+                done
+            } >>"$bash_config"
+
+            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
+
+            refresh_command="source $bash_config"
+            set_manually=false
+            break
+        fi
+    done
+
+    if [[ $set_manually = true ]]; then
+        echo "Manually add the directory to $tilde_bash_config (or similar):"
+
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
     ;;
 *)
-    echo "Your shell (${SHELL##*/}) is not explicitly supported for automatic PATH update."
-    echo "Please add $bin_dir to your PATH manually."
+    echo 'Manually add the directory to ~/.bashrc (or similar):'
+    info_bold "  export $install_env=$quoted_install_dir"
+    info_bold "  export PATH=\"$bin_env:\$PATH\""
     ;;
 esac
 
-# Final success message
-success "aitdd was installed successfully. Please restart your terminal or source your shell config for the changes to take effect."
+echo
+info "To get started, run:"
+echo
+
+if [[ $refresh_command ]]; then
+    info_bold "  $refresh_command"
+fi
+
+info_bold "  aitdd --help"
